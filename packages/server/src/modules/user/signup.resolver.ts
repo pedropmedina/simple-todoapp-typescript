4ddb7c94890f7a6148c8228a37f7cd117bb2c63d
pedropmedina <pedrodelilia@gmail.com>
2019-12-17 23:36:01 -0500

    Set cookie during signup and add Context interface

--- packages/server/src/modules/user/signup.resolver.ts
+++ packages/server/src/modules/user/signup.resolver.ts
@@ -1,16 +1,25 @@
-import { Resolver, Mutation, Arg, FieldResolver, Root } from 'type-graphql';
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  FieldResolver,
+  Root,
+  Ctx
+} from 'type-graphql';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../../entity/User';
 import { SingupInput } from './signup.input';
 import { Auth } from './auth.type';
+import { Context } from '../../types/context';
 
 @Resolver(() => User)
 export class SignupResolver {
   @Mutation(() => Auth)
   async signup(
-    @Arg('input') { firstName, lastName, email, password }: SingupInput
+    @Arg('input') { firstName, lastName, email, password }: SingupInput,
+    @Ctx() { res }: Context
   ): Promise<Auth> {
     // check for user in db
     let user: User | undefined = await User.findOne({ email });
@@ -33,6 +42,12 @@ export class SignupResolver {
     // create jwt and send it in the headers
     const token: string = jwt.sign({ userId: user.id }, 'kl;jsafl;jafl;kj');
     console.log(token);
+
+    // set cookie
+    res.cookie('jrt', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    });
 
     return { user, token };
   }
